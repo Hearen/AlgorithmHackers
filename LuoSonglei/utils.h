b4bf163819ade6454fd823c5451bfffee19b339d
LHearen <lhearen@126.com>
2015-12-01 08:20:35 +0800

    original commit

---
+++ LuoSonglei/utils.h
@@ -0,0 +1,45 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+#define SIZE 2000000
+#define MAX 10000000
+
+#define minP(pa, pb) (*(pa) > *(pb) ? (pb) : (pa))
+#define maxP(pa, pb) (*(pa) > *(pb) ? (pa) : (pb))
+#define min(a, b) (a) < (b) ? (a) : (b)
+#define max(a, b) (a) > (b) ? (a) : (b)
+
+extern int STACK[SIZE];
+extern int INDEX;
+int aux[SIZE];
+
+void stackInit();
+int pop();
+void push(int l, int r);
+bool isEmpty();
+
+void randomIntArray(int* array, int size, int low, int high);
+void printArray(int *nums, int size);
+void swap(int*p, int*q);
+void checkAscending(int *nums, int size);
+
+void shakerSort(int * nums, int size);
+void selectionSort(int *nums, int size);
+void bubbleSort0(int * nums, int size);
+void bubbleSort1(int * nums, int size);
+void insertionSort(int* nums, int l, int r);
+void insertionSort0(int* nums, int size);
+void insertionSort1(int* nums, int size);
+void insertionSort2(int* nums, int size);
+void insertionSort3(int* nums, int size);
+void shellSort0(int *nums, int size);
+void shellSort1(int *nums, int size);
+void keySort(int *nums, int size, int keySize);
+
+void merge(int *a, int l, int m, int r);
+void mergeAB(int *a, int *aux, int l, int m, int r);
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This repository is used by ISCAS14 only to practise algorithms from Leetcode; under each directory there will be a set of personal solutions to all the same set of problems and the solutions under leetcode will be the ones that are either classic or well-tuned or well-coded.
+#Attention
+All the arrangements will be updated before 21:30 in each monday and the corresponding personal solutions for that week will be handed in by the end of 24:00 in friday.
