26b9a1e6e015fbcf6159f2ef3bd67b062be4a77e
LHearen <lhearen@126.com>
2015-12-06 11:09:35 +0800

    .gitignore refreshed

--- LuoSonglei/Subsets.c
+++ LuoSonglei/Subsets.c
@@ -8,27 +8,42 @@ Source: https://leetcode.com/problems/subsets/
 //*returnSize will be halfed after each iteration;
 //index is used to follow the position in nums;
 //setIndex is used to follow the position in allSets;
-void combination(int* nums, int numsSize, int** columnSizes, int* returnSize, int **allSets, int index, int setIndex)
+#include <stdio.h>
+#include<stdlib.h>
+#define TEST
+void combination(int* nums, int numsSize, int* columnSizes, int baseIndex, int returnSize, int **allSets, int index, int setIndex)
 {
+    #ifdef TEST
+        printf("numsIndex: %d\t setIndex: %d\n", index, setIndex);
+        printf("columnSizes %d:\t%d\n",columnSizes[returnSize]);
+    #endif
     if(index == numsSize)
     {
-        *columnSizes[*returnSize] = setIndex;
         return;
     }
-    for(int i = 0; i < *returnSize; i++)
+    
+    #ifdef TEST
+        printf("returnSize: %d\n", returnSize);
+    #endif
+    for(int i = 0; i < returnSize; i++)
     {
-        if(i < *returnSize/2)
+        if(i < returnSize/2)
         {
-            combination(nums, numsSize, columnSizes, *returnSize/2, allSets, index + 1, setIndex + 1);
-            allSets[i] = nums[index];
+            *allSets[i] = nums[index];
+            #ifdef TEST
+                printf("allSets %d:\t%d\n", *allSets[i], i);
+                printf("setIndex: %d\n",setIndex);
+            #endif
+            columnSizes[baseIndex + i] = setIndex;
+            combination(nums, numsSize, columnSizes, baseIndex + returnSize/2, returnSize/2, allSets, index + 1, setIndex + 1);
         }
         else
         {
-            combination(nums, numsSize, columnSizes, *returnSize/2, allSets, index + 1, setIndex);
+            combination(nums, numsSize, columnSizes, baseIndex, returnSize/2, allSets, index + 1, setIndex);
         }
     }
 }
-int **subsets(int* nums, int numsSize, int** columnSizes, int* returnSize)
+int **subsets(int* nums, int numsSize, int* columnSizes, int* returnSize)
 {
     int total = 1;
     int count = 0;
@@ -37,11 +52,30 @@ int **subsets(int* nums, int numsSize, int** columnSizes, int* returnSize)
         total *= 2;
         count++;
     }
-    columnSizes = (int*)malloc(sizeof(int*) * total);
+    #ifdef TEST
+        printf("%d\n", total);
+    #endif
+    columnSizes = (int*)malloc(sizeof(int) * total);
     *returnSize = total;
-    int **allSets = (int*)malloc(sizeof(int*) * total);
+    int **allSets = (int**)malloc(sizeof(int*) * total);
     for(int i = 0; i < total; i++)
         allSets[i] = (int*)malloc(sizeof(int) * numsSize);
-    combination(nums, numsSize, columnSizes, returnSize, allSets, 0, 0);
+    combination(nums, numsSize, columnSizes, 0, *returnSize, allSets, 0, 0);
     return allSets;
+}
+
+int main(int argc, char *argv[])
+{
+    int nums[] = {0};
+    int *columnSizes;
+    int *returnSize;
+    int **sets;
+    sets = subsets(nums, sizeof(nums) / sizeof(int), columnSizes, returnSize);
+    for(int i = 0; i < *returnSize; i++)
+    {
+        for(int j = 0; j < columnSizes[i]; j++)
+            printf("%d\t", sets[j]);
+        printf("\n");
+    }
+    return 0;
 }
