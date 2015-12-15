f50dca8703e8117e517e0e00ea2fefa99b795b7d
LHearen <lhearen@126.com>
2015-12-15 09:24:50 +0800

    Str2Int added

---
+++ LuoSonglei/week-3/String2Integer.c
@@ -0,0 +1,75 @@
+/*******************************************
+Author: LHearen
+E-mail: LHearen@126.com
+Time  :	2015-12-15 08:27
+Description: 
+Source: https://leetcode.com/problems/string-to-integer-atoi/
+*******************************************/
+//handle three different types octal, decimal and hexidecimal
+int myAtoi1(char* s)
+{
+    int unit = 10;
+    int index = 0;
+    int size = 0;
+    while(*(s+size) != '\0' && isxdigit(*(s+size)))
+        size++;
+    if(isdigit(*(s)))
+    {
+        if(*s - '0' == 0)
+        {
+            index = 1;
+            unit = 8;
+        }
+        if(size > 1 && (*(s+1) == 'x' || *(s+1) == 'x'))
+        {
+            index = 2;  
+            unit = 16;
+        }
+    }
+    else
+    {
+        return 0;
+    }
+    int sum = 0;
+    int cell = 0;
+    for(; index < size; index++)
+    {
+        if(unit == 16)
+        {
+            if(isxdigit(*(s+index)))
+                cell = (isalpha(*(s+index)) ? tolower(*(s+index)) - 'a' + 10 : *(s+index) - '0');
+            else
+                break;
+        }
+        else
+            cell =  *(s+index) - '0';
+        sum = unit * sum + cell;
+    }
+    return sum; 
+}
+
+
+int myAtoi(char* s)
+{
+    int i = 0;
+    int flag = 1;
+    while(isblank(*(s+i)) || isblank(*(s+i)))
+        i++;
+    if(*(s+i) == '-' || *(s+i) == '+')
+    {
+        if(*(s+i) == '-')
+            flag = -1;
+        i++;
+    }
+    long sum = 0;
+    for(; isdigit(*(s+i)); i++)
+    {
+        sum = 10 * sum + *(s+i) - '0';
+        if(sum >> 32 > 0)
+            break;
+    }
+    sum *= flag;
+    sum = sum > INT_MAX ? INT_MAX : sum;
+    sum = sum < INT_MIN ? INT_MIN : sum;
+    return sum;
+}
