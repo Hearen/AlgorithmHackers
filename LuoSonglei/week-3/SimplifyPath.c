71d10931f16b8814aa10a8e16a4f62fcd4cceec1
LHearen <lhearen@126.com>
2015-12-17 08:39:29 +0800

    SimplifyPath corrected

--- LuoSonglei/week-3/SimplifyPath.c
+++ LuoSonglei/week-3/SimplifyPath.c
@@ -8,40 +8,55 @@ Source      : https://leetcode.com/problems/simplify-path/
 
 char* simplifyPath(char* path)
 {
-    int pre = -1, cur = 0;
+    int pre = 0, cur = 0;
     int index = 0;
     int size = strlen(path);
     if(size == 0)
         return "";
+    //make sure the last character is a forward slash;
     if(*(path+size-1) != '/')
     {
-        strcat(path, "/");
+        /*strcat(path, "/");//the space spared for path should be enough for another character - but in this case, it's not reliable;*/
+        char* tmp = (char*)malloc(sizeof(char)*(size+2));
+        strcpy(tmp, path);
+        strcat(tmp, "/");
+        path = tmp;
         size++;
     }
+    //make sure the pre and cur point to the first forward slash;
+    while(*(path+cur) != '/')
+    {
+        pre++;
+        cur++;
+    }
     int unitSize = 0;
-    char* unit = (char*)malloc(sizeof(char)*size*2);
+    char* unit = (char*)malloc(sizeof(char)*(size+1));
     char** sArray = (char**)malloc(sizeof(char*) * size);
     for(; *(path+cur) != '\0'; cur++)
     {
         if(*(path+cur) == '/')
         {
             unitSize = cur - pre - 1;
+            if(unitSize < 0)//pre and cur point to the same position;
+                continue;
             strncpy(unit, path+pre+1, unitSize);
             unit[unitSize] = '\0';
             pre = cur;
-            if(strcmp(unit, ".") == 0) //stay the same;
+            if(strcmp(unit, ".") == 0 || strcmp(unit, "") == 0) //stay the same;
                 continue;
             if(strcmp(unit, "..") == 0) //pop out;
-                index > 0? index-- : (index = 0);
+            {
+                if(index > 0)
+                    index--;
+            }
             else
             {
-                sArray[index] = (char*)malloc(sizeof(char) * 2 * unitSize);
+                sArray[index] = (char*)malloc(sizeof(char) * (unitSize+1));
                 strcpy(sArray[index++], unit);
             }
         }
     }
-    if(index == 0)
-        return "";
+    //collecting the directories;
     unit[0] = '/';
     unit[1] = '\0';
     unitSize = 0;
@@ -51,6 +66,7 @@ char* simplifyPath(char* path)
         strcat(unit, sArray[i]);
         strcat(unit, "/");
     }
-    unit[unitSize] = '\0';
+    if(index > 0) //overwriting the last forward slash if it's not root;
+        unit[unitSize] = '\0';
     return unit;
 }
