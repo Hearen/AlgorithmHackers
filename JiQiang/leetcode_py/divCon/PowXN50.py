c446b016f71b051a061c44cf74c7a60cc52b5a68
qiang2010 <qiangji1991@gmail.com>
2015-12-25 23:00:00 +0800

    20151225 week 4 jq

--- JiQiang/leetcode_py/divCon/PowXN50.py
+++ JiQiang/leetcode_py/divCon/PowXN50.py
@@ -17,8 +17,8 @@ class Solution(object):
         #     return  1.0/self.myPowPositiveN(x,-n)
         # return  self.myPowPositiveN(x,n)
         if n < 0:
-            return  1.0/self.myPowDiv(x,-n)
-        return  self.myPowDiv(x,n)
+            return  1.0/self.myPow3(x,-n)
+        return  self.myPow3(x,n)
 
     def myPowPositiveN(self,x,posiN):
 
@@ -43,6 +43,19 @@ class Solution(object):
         right =self.myPowDiv(x,n-n//2)
         return  left*right
 
+    '''
+    这里根据位来计算。时间复杂度是log(N)
+    '''
+    def myPow3(self,x,n):
+        base = x
+        ans = 1
+        while n > 0:
+            bi = n &1
+            if bi == 1:
+                ans *= base
+            base *=base
+            n>>=1
+        return  ans
 
 if __name__ == '__main__':
 
