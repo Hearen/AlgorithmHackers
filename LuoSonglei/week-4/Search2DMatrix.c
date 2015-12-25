5a250bd7929892200de6739d3fdfdc2f674007f4
LHearen <lhearen@126.com>
2015-12-25 09:38:43 +0800

    Search2DMatrix updated

--- LuoSonglei/week-4/Search2DMatrix.c
+++ LuoSonglei/week-4/Search2DMatrix.c
@@ -7,7 +7,7 @@ Source      : https://leetcode.com/problems/search-a-2d-matrix/
 *******************************************/
 #include<stdbool.h>
 //AC - 8ms;
-bool searchMatrix(int** matrix, int rowSize, int colSize, int target)
+bool searchMatrix0(int** matrix, int rowSize, int colSize, int target)
 {
     int rLow = 0, rHigh = rowSize-1, rMid = 0;
     while(rLow < rHigh)
@@ -33,4 +33,39 @@ bool searchMatrix(int** matrix, int rowSize, int colSize, int target)
             return true;
     } 
     return false;
+}
+
+//AC - 8ms;
+bool searchMatrix1(int** matrix, int rowSize, int colSize, int target)
+{
+    int low = 0, high = rowSize*colSize - 1, mid;
+    int t = 0;
+    while(low <= high)
+    {
+        mid = (low + high) >> 1;
+        t = matrix[mid / colSize][mid % colSize];
+        if(t > target)
+            high = mid - 1;
+        else if(t < target) 
+            low = mid + 1;
+        else
+            return true;
+    }
+    return false;
+}
+
+
+//AC - 8ms;
+bool searchMatrix2(int** matrix, int rowSize, int colSize, int target)
+{
+    int low = 0, high = rowSize*colSize - 1, mid;
+    while(low != high)
+    {
+        mid = (low+high) >> 1;
+        if(matrix[mid / colSize][mid % colSize] < target)
+            low = mid + 1;
+        else
+            high = mid;
+    } 
+    return matrix[low / colSize][low % colSize] == target;
 }
