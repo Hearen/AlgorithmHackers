49dc58e8973283b793ac6f9735bc3684132c6549
LHearen <lhearen@126.com>
2016-01-02 14:27:07 +0800

    N-Queens updated

--- LuoSonglei/week-5/N-Queens.c
+++ LuoSonglei/week-5/N-Queens.c
@@ -2,7 +2,7 @@
 Author      : LHearen
 E-mail      : LHearen@126.com
 Time        : 2016-01-02 10:06
-Description : 
+Description : The n-queens puzzle is the problem of placing n queens on an n×n chessboard such that no two queens attack each other. Given an integer n, return all distinct solutions to the n-queens puzzle. Each solution contains a distinct board configuration of the n-queens' placement, where 'Q' and '.' both indicate a queen and an empty space respectively.'
 Source      : https://leetcode.com/problems/n-queens/
 *******************************************/
 #include <stdio.h>
