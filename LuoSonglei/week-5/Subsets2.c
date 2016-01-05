e18b86d4c46df14eec59ef9f31fc9a08358feaf7
LHearen <lhearen@126.com>
2016-01-05 21:22:34 +0800

    Subset2 updated with 4ms

--- LuoSonglei/week-5/Subsets2.c
+++ LuoSonglei/week-5/Subsets2.c
@@ -6,13 +6,16 @@ Description :
 Source      : https://leetcode.com/problems/subsets-ii/
 *******************************************/
 #define FOO
-void search(int index, int* nums, int size, int* stack, int top, int** columnSizes, int* count, int** table)
+void search0(int index, int* nums, int size, int* stack, int top, int** columnSizes, int* count, int** table)
 {
     #ifdef FOO
         printf("inside search index: %d\tsize: %dtop: %d\n", index, size, top);
     #endif
     if(index == size)
     {
+        #ifdef FOO
+            printf("Before allocating for table[*count] count: %d\sizee of table: %d\n", *count, sizeof(table)/sizeof(int*));
+        #endif
         table[*count] = (int*)malloc(sizeof(int)*(top+1));
         #ifdef FOO
             printf("Before collecting...\n");
@@ -48,7 +51,7 @@ void search(int index, int* nums, int size, int* stack, int top, int** columnSiz
     search(index+1, nums, size, stack, top, columnSizes, count, table);
 }
 
-int** subsetsWithDup(int* nums, int size, int** columnSizes, int* returnSize)
+int** subsetsWithDup0(int* nums, int size, int** columnSizes, int* returnSize)
 {
     int i, min = 0;
     for(i = 0; i < size; i++)
@@ -71,10 +74,71 @@ int** subsetsWithDup(int* nums, int size, int** columnSizes, int* returnSize)
         for(i = 0; i < size; i++)
             printf("%d\n", nums[i]);
     #endif
+    *returnSize = 1;
+    for(int i = 0; i < size; i++)
+        *returnSize *= 2;
+    #ifdef FOO
+        printf("returnSize: %d\n", *returnSize);
+    #endif
     *columnSizes = (int*)malloc(sizeof(int) * (*returnSize));
     int** table = (int**)malloc(sizeof(int*) * (*returnSize));
     int* stack = (int*)malloc(sizeof(int)*(*returnSize));
     int top = -1;
-    search(0, nums, size, stack, top, columnSizes, returnSize, table);
+    int* count = 0;
+    search(0, nums, size, stack, top, columnSizes, count, table);
+    return table;
+}
+
+void search(int* nums, int size, int begin, int* stack, int top, int** columnSizes, int* count, int** table)
+{
+    if(begin != 0 && top == -1)
+        return;
+    (*columnSizes)[*count] = top+1;
+    table[*count] = (int*)malloc(sizeof(int)*(top+1));
+    for(int i = 0; i <= top; i++)
+        table[*count][i] = stack[i];
+    (*count)++;
+    for(int i = begin; i < size; i++)
+    {
+        if(i == begin || nums[i] != nums[i-1])
+        {
+            stack[++top] = nums[i];
+            search(nums, size, i+1, stack, top, columnSizes, count, table);
+            top--;
+        }
+    }
+}
+
+//AC - 4ms;
+int** subsetsWithDup(int* nums, int size, int** columnSizes, int* returnSize)
+{
+    int i, min = 0;
+    for(i = 0; i < size; i++)
+        if(nums[i] < nums[min])
+            min = i;
+    int t = nums[min];
+    nums[min] = nums[0];
+    nums[0] = t;
+    for(i = 1; i < size; i++)
+    {
+        t = nums[i];
+        int j = i;
+        while(t < nums[j-1])
+        {
+            nums[j] = nums[--j];
+        }
+        nums[j] = t;
+    }
+    *returnSize = 1;
+    for(int i = 0; i < size; i++)
+        *returnSize *= 2;
+    *columnSizes = (int*)malloc(sizeof(int)*(*returnSize));
+    int** table = (int**)malloc(sizeof(int*)*(*returnSize));
+    int* stack = (int*)malloc(sizeof(int)*size);
+    int top = -1;
+    int tmp = 0;
+    int* count = &tmp;
+    search(nums, size, 0, stack, top, columnSizes, count, table);
+    *returnSize = *count;
     return table;
 }
