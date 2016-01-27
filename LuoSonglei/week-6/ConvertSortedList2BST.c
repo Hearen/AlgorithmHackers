d90c37b7da9cf3882e2c2355f60fbb7e3b0f992a
LHearen <lhearen@126.com>
2016-01-27 15:59:43 +0800

    ConvertSortedList2BST.c updated

--- LuoSonglei/week-6/ConvertSortedList2BST.c
+++ LuoSonglei/week-6/ConvertSortedList2BST.c
@@ -20,7 +20,7 @@ struct TreeNode
 };
 
 //AC - 12ms;
-struct TreeNode* sortedListToBST(struct ListNode* head)
+struct TreeNode* sortedListToBST0(struct ListNode* head)
 {
     if(head == NULL)
         return NULL;
@@ -44,5 +44,32 @@ struct TreeNode* sortedListToBST(struct ListNode* head)
     root->val = right->val;
     root->left = sortedListToBST(left);
     root->right = sortedListToBST(right->next);
+    return root;
+}
+
+//AC - 8ms;
+struct TreeNode* sortedListToBST1(struct ListNode* head)
+{
+    if(head == NULL)
+        return NULL;
+    if(head->next == NULL)
+    {
+        struct TreeNode *t = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        t->right = t->left = NULL;
+        t->val = head->val;
+        return t;
+    }
+    struct ListNode* slow = head;
+    struct ListNode* fast = head->next->next;
+    while(fast && fast->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = slow->next->val;
+    root->right = sortedListToBST(slow->next->next);
+    slow->next = NULL;
+    root->left = sortedListToBST(head);
     return root;
 }
