ccb29f3d9f998352b666245914dfc5809532c799
Hearen <LHearen@126.com>
2016-02-13 20:28:33 +0800

    PalindromePartition2.c updated with comments

--- LuoSonglei/week-91/PalindromePartition2.c
+++ LuoSonglei/week-91/PalindromePartition2.c
@@ -11,19 +11,19 @@ int minCut(char *s)
 {
     int len = strlen(s);
     if(len == 0) return 0;
-    bool **isPal = (int**)malloc(sizeof(bool*)*len);
+    bool **isPal = (int**)malloc(sizeof(bool*)*len); //Used to record the whether the substring from i to j inclusive is a palindrome or not;
     for(int i=0; i<len; i++)
     {
         isPal[i] = (bool*)malloc(sizeof(bool)*len);
         memset(isPal[i], 0, sizeof(bool)*len);
     }
-    int *minCuts = (int*)malloc(sizeof(int)*len);
+    int *minCuts = (int*)malloc(sizeof(int)*len); //record the minimal cuts in the substring from index till end;
     for(int i=len-1; i > -1; i--)
     {
-        minCuts[i]=len-i-1;
-        for(int j=i; j<len; j++)
+        minCuts[i]=len-i-1; //the default minimal cuts;
+        for(int j=i; j<len; j++) //to find the minimal cuts from the current index till end;
         {
-            if(s[i]==s[j] && (j-i<2 || isPal[i+1][j-1]))
+            if(s[i]==s[j] && (j-i<2 || isPal[i+1][j-1])) //once it's a palindrome from i to j inclusive;
             {
                 isPal[i][j] = true;
                 if(j==len-1)
