aa9f74436b49cc5efcf8dd1b6af0badfb4293b26
LHearen <lhearen@126.com>
2016-02-13 11:49:04 +0800

    MaxSquare.c added

---
+++ LuoSonglei/week-10/MaxSquare.c
@@ -0,0 +1,45 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-02-13 10:58
+Description : 
+Source      : https://leetcode.com/problems/maximal-square/
+*******************************************/
+//AC - 4ms;
+int maximalSquare(char** matrix, int rSize, int cSize)
+{
+    int max = 0;
+    int *pre = (int*)malloc(sizeof(int)*cSize);
+    int *cur = (int*)malloc(sizeof(int)*cSize);
+    for(int r=0; r<rSize; r++)
+        for(int c=0; c<cSize; c++)
+        {
+            if(c==0)
+                cur[c] = matrix[r][c] - '0';
+            else
+            {
+                if(matrix[r][c] == '1')
+                {
+                    if(r > 0)
+                    {
+                        int t = cur[c-1];
+                        if(pre[c] < t)
+                            t = pre[c];
+                        if(pre[c-1] < t)
+                            t = pre[c-1];
+                        cur[c] = t+1;
+                    }
+                    else
+                        cur[c] = 1;
+                }
+                else
+                    cur[c] = 0;
+            }
+            if(cur[c] > max)
+                max = cur[c];
+        }
+    int *t = pre;
+    pre = cur;
+    cur = t;
+    return max*max;
+}
