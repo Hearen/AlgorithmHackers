c438de328fcebb1058514bc56493f16b23a25224
Hearen <LHearen@126.com>
2016-02-15 22:28:48 +0800

    ScrambleString.c updated

--- LuoSonglei/week-92/ScrambleString.c
+++ LuoSonglei/week-92/ScrambleString.c
@@ -31,10 +31,40 @@ bool isScramble0(char* s1, char* s2, int len)
 }
 
 //AC - 0ms - beats 100% submissions;
-bool isScramble(char* s1, char* s2)
+bool isScramble1(char* s1, char* s2)
 {
     int len1 = strlen(s1), len2 = strlen(s2);
     if(len1 != len2) return false;
     int len = len1;
     return isScramble0(s1, s2, len);
+}
+
+//AC - 24ms - beats 100% submissions - DP solution;
+bool isScramble(char* s1, char* s2)
+{
+    int len = strlen(s1);
+    if(!len) return true;
+    if(len==1) return *s1==*s2;
+    bool*** match = (bool***)malloc(sizeof(bool**)*(len+1));
+    for(int i = 0; i <= len; i++)
+    {
+        match[i] = (bool**)malloc(sizeof(bool*)*len);
+        for(int j = 0; j < len; j++)
+        {
+            match[i][j] = (bool*)malloc(sizeof(bool)*len);
+            memset(match[i][j], 0, sizeof(bool)*len);
+        }
+    }
+    for(int i = 0; i < len; i++)
+        for(int j = 0; j < len; j++)
+            match[1][i][j] = (s1[i] == s2[j]);
+    for(int size = 2; size <= len; size++)
+        for(int i = 0; i <= len-size; i++)
+            for(int j = 0; j <= len-size; j++)
+            {
+                match[size][i][j] = false;
+                for(int k = 1; k<size && !match[size][i][j]; k++)
+                    match[size][i][j] = (match[k][i][j] && match[size-k][i+k][j+k]) || (match[k][i+size-k][j] && match[size-k][i][j+k]);
+            }
+    return match[len][0][0];
 }
