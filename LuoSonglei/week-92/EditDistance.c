c3e980e949d2328908b4281f4955f3be22cd7617
LHearen <lhearen@126.com>
2016-02-17 10:40:42 +0800

    EditDistance.c updated with only one single array to replace two-dimension one to store the states

--- LuoSonglei/week-92/EditDistance.c
+++ LuoSonglei/week-92/EditDistance.c
@@ -14,7 +14,7 @@ int min(int a, int b, int c)
 }
 
 //AC - 12ms;
-int minDistance(char* s1, char* s2)
+int minDistance0(char* s1, char* s2)
 {
     int len1=strlen(s1), len2=strlen(s2);
     int **mins = (int**)malloc(sizeof(int*)*(len1+1));
@@ -36,4 +36,26 @@ int minDistance(char* s1, char* s2)
                 mins[i][j] = min(mins[i-1][j], mins[i][j-1], mins[i-1][j-1]) + 1;
         }
     return mins[len1][len2];
+}
+
+//AC - 12ms;
+int minDistance(char* s1, char* s2)
+{
+    int len1=strlen(s1), len2=strlen(s2);
+    int *cur = (int*)malloc(sizeof(int)*(len2+1));
+    int pre = 0;
+    for(int i = 0; i <= len2; i++)
+        cur[i] = i;
+    for(int i = 1; i <= len1; i++)
+    {
+        pre = cur[0];
+        for(int j = 1; j <= len2; j++)
+        {
+            int t = cur[j];
+            cur[j] = s1[i-1] == s2[j-1]? pre : min(cur[j-1], cur[j], pre) + 1;
+            pre = t;
+        }
+        cur[0] = i;
+    }
+    return cur[len2];
 }
