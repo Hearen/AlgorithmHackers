1f16603c948d22936673d768d66aeb2f3dd8af6c
LHearen <lhearen@126.com>
2016-02-18 10:47:01 +0800

    WordBreak2.c still with error

--- LuoSonglei/week-92/WordBreak2.c
+++ LuoSonglei/week-92/WordBreak2.c
@@ -11,58 +11,67 @@ Source      : https://leetcode.com/problems/word-break-ii/
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 bool exist(char* s, int len, char** wordDict, int size)
 {
-    /*printf("inside exist\n");*/
-    /*for(int i = 0; i < len; i++)*/
-        /*printf("%c", s[i]);*/
-    /*printf("\nleaving exist\n");*/
     for(int i = 0; i < size; i++)
-        if(!strncmp(wordDict[i], s, len))
+    {
+        if(strlen(wordDict[i]) == len && !strncmp(wordDict[i], s, len))
             return true;
+    }
     return false;
 }
 
-char* merge(char* s, int len, char* cur, int total)
+char* merge(char* s, int len, const char* cur, int total, int space)
 {
     printf("inside merge -- cur: %s\t len: %d\n", cur, strlen(cur));
-    char* t = (char*)malloc(sizeof(char)*(total));
+    char* t = (char*)malloc(sizeof(char)*(total+space));
     *t = '\0';
     printf("len: %d\t%s\n", strlen(t), t);
     strcat(t, cur);
     printf("len: %d\t%s\n", strlen(t), t);
     strncat(t, s, len);
+    if(space) strcat(t, " ");
     printf("len: %d\t%s\n", strlen(t), t);
+    return t;
 }
 
 void buildPath(bool* breakable, char* s, int pos, char** arr, int* returnSize, char* cur, char** wordDict, int size, int min, int max)
 {
-    printf("inside buildPath -- cur: %s\t len: %d\n", cur, strlen(cur));
+    printf("begin inside buildPath -- cur: %s\t len: %d\n", cur, strlen(cur));
+    printf("current size: %d\n", *returnSize);
+    for(int i = 0; i < *returnSize; i++)
+        printf("%s\n", arr[i]);
     int len = strlen(s);
     for(int i = min; i <= MIN(max, len-pos); i++)
     {
-        int total = i+strlen(cur)+2;
+        int total = i+strlen(cur)+1;
+        printf("checking len: %d\tmax: %d\tend: %d\n", i, max, len-pos);
         if(breakable[i+pos] && exist(s+pos, i, wordDict, size))
         {
-            printf("\nexists in wordDict\n");
+            printf("pre: %s\n", cur);
+            printf("exists in wordDict\n");
             for(int j = 0; j < i; j++)
                 printf("%c", s[pos+j]);
             printf("\n");
-            if(*(s+pos+i) == '\0')
+            if( pos+i == len)
             {
                 (*returnSize)++;
                 arr = (char**)realloc(arr, sizeof(char*)*(*returnSize));
-                arr[*returnSize-1] = merge(s+pos, i, cur, total);
+                arr[*returnSize-1] = merge(s+pos, i, cur, total, 0);
+                printf("---------ends here a new string: new size: %d\n", *returnSize);
+                for(int i = 0; i < *returnSize; i++)
+                    printf("%s\n", arr[i]);
+                printf("\n\n");
             }
             else 
             {
-                printf("inside buildPath -- cur: %s\t len: %d\n", cur, strlen(cur));
-                char* cur = merge(s+pos, i, cur, total);
-                cur[total-2] = ' ';
-                cur[total-1] = '\0';
-                buildPath(breakable, s, pos+i, arr, returnSize, cur, wordDict, size, min, max);
+                printf("processing inside buildPath -- cur: %s\t len: %d\n", cur, strlen(cur));
+                buildPath(breakable, s, pos+i, arr, returnSize, merge(s+pos, i, cur, total, 1), wordDict, size, min, max);
             }
         }
     }
 }
+
+//RE - when hitting size - 5; 
+//"aaaaaaa" ["aaaa","aa","a"]
 char** wordBreak(char* s, char** wordDict, int size, int* returnSize)
 {
     char** arr = (char**)malloc(sizeof(char*));
