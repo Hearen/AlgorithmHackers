3f60cfe7f73685fa15211e1ab1881443ae913bad
LHearen <lhearen@126.com>
2016-02-20 09:02:09 +0800

    ConvertSortedArray2HeightBalancedBST.c updated with another method

--- LuoSonglei/week-92/ConvertSortedArray2HeightBalancedBST.c
+++ LuoSonglei/week-92/ConvertSortedArray2HeightBalancedBST.c
@@ -19,6 +19,20 @@ struct TreeNode* sortedArrayToBST(int* nums, int size)
     struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
     root->val = nums[mid];
     root->left = sortedArrayToBST(nums, mid);
-    root->right = sortedArrayToBST(nums+mid+1, size-mid-1);
+    root->right = sortedArrayToBST(nums+mid+1, size-mid-1); 
+    return root;
+}
+
+//AC - 4ms;
+struct TreeNode* sortedArrayToBST(int* nums, int size)
+{
+    if(!size) return NULL;
+    int mid = size>>1; //get the mid of the index part instead of size;
+    struct TreeNode* root = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    root->val = nums[mid];
+    root->left=root->right=NULL;
+    if(size==1) return root;
+    root->left = sortedArrayToBST(nums, mid);
+    root->right = sortedArrayToBST(nums+mid+1, size-mid-1); //if size==1 nums+mid+1 will cross the line and result as error;
     return root;
 }
