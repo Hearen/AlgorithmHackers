7d7b478c3e84b7612353bf396a83a76b87cd55e6
LHearen <lhearen@126.com>
2016-02-22 09:26:46 +0800

    GasStation.c updated with comments

--- LuoSonglei/week-92/GasStation.c
+++ LuoSonglei/week-92/GasStation.c
@@ -5,21 +5,20 @@ Time        : 2016-02-20 20:42
 Description : 
 Source      : https://leetcode.com/problems/gas-station/
 *******************************************/
+//AC - 0ms;
+//https://leetcode.com/discuss/87765/clean-simple-solution-well-explained-accepted-submission
 int canCompleteCircuit(int* gas, int gSize, int* cost, int cSize)
 {
-    int left = 0;
-    for(int i = 0; i < gSize; i++)
+    int index=-1, total=0, max=-1;
+    for(int i = cSize-1; i > -1; i--)
     {
-        int j, count;
-        for(j=0, count=i; j < cSize; j++, count++)
+        total += gas[i]-cost[i];
+        if(total > max)
         {
-            left += gas[count%gSize];
-            left -= cost[j];
-            if(left < 0)
-                break;
+            index=i;
+            max=total;
         }
-        if(j == cSize)
-            return 1;
     }
-    return -1;
+    return total < 0? -1 : index;
 }
+
