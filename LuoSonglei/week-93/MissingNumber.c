7e7cf1f6c71dea2f334eb2896a850ab03152b823
Hearen <LHearen@126.com>
2016-02-27 21:43:59 +0800

    MissingNumber.c added and updated

---
+++ LuoSonglei/week-93/MissingNumber.c
@@ -0,0 +1,28 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-02-27 21:35
+Description : 
+Source      : https://leetcode.com/problems/missing-number/
+*******************************************/
+#include <bool.h>
+//AC - 12ms - using O(n) space;
+int missingNumber0(int* nums, int size)
+{
+    bool *arr = (int*)malloc(sizeof(bool)*(size+1));
+    memset(arr, 0, sizeof(bool)*(size+1));
+    for(int i = 0; i < size; i++)
+        arr[nums[i]] = true;
+    for(int i = 0; i <= size; i++)
+        if(!arr[i])
+            return i;
+}
+
+//AC - 12ms;
+int missingNumber(int* nums, int size)
+{
+    int t = 0;
+    for(int i = 0; i < size; i++)
+        t ^= nums[i]^i;
+    return t^size;
+}
