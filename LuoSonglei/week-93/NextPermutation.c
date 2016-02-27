15418d07f1b49af23025ae82a76f8f675c1b2ec9
Hearen <LHearen@126.com>
2016-02-27 10:53:29 +0800

    NextPermutation.c updated with comments

--- LuoSonglei/week-93/NextPermutation.c
+++ LuoSonglei/week-93/NextPermutation.c
@@ -7,9 +7,7 @@ Source      : https://leetcode.com/problems/next-permutation/
 *******************************************/
 void swap(int* p, int* q)
 {
-    int t = *p;
-    *p = *q;
-    *q = t;
+    int t = *p; *p = *q; *q = t;
 }
 void reverse(int* nums, int begin, int end)
 {
@@ -19,13 +17,13 @@ void reverse(int* nums, int begin, int end)
 void nextPermutation(int* nums, int size)
 {
     int i=size-1, j=size-1;
-    while(i>0 && nums[i]<=nums[i-1]) i--;
-    if(i==0)
+    while(i>0 && nums[i]<=nums[i-1]) i--; //make sure the [i..size-1] is in descending order;
+    if(i==0) //the whole array is descending now, reverse it to the smallest as problem requires;
     {
         reverse(nums, 0, size-1);
         return ;
     }
-    while(nums[j] <= nums[i-1]) j--;
-    swap(nums+j, nums+i-1);
-    reverse(nums, i, size-1);
+    while(nums[j] <= nums[i-1]) j--; //find the first bigger one backwards;
+    swap(nums+j, nums+i-1); //ensure the next is bigger;
+    reverse(nums, i, size-1); //since [i..size-1] is descending, after reverse it will be ascending and as a result - [i..size-1] will be the smallest - the smallest in the bigger results - the next permutation;
 }
