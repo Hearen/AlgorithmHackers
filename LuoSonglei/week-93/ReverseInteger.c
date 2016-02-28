f37142ac4090800a7995596fc95d9ad2467640e1
Hearen <LHearen@126.com>
2016-02-28 15:53:02 +0800

    ReverseInteger.c added

--- LuoSonglei/week-93/ReverseInteger.c
+++ LuoSonglei/week-93/ReverseInteger.c
@@ -9,15 +9,15 @@ Source      : https://leetcode.com/problems/reverse-integer/
 //AC - 4ms;
 int reverse(int n)
 {
-    if(n==INT_MIN || n==INT_MAX) return 0;
+    if(n==INT_MIN || n==INT_MAX) return 0; //two extreme values are ignored;
     bool negative = n<0? true : false;
     n = negative? -n : n;
     long long a = 0;
-    while(n)
+    while(n) //reverse n;
     {
         a = 10*a + n%10;
         n /= 10;
     }
-    if(a > INT_MAX) return 0;
+    if(a > INT_MAX) return 0; //in case of reversed big number;
     return negative? -a : a;
 }
