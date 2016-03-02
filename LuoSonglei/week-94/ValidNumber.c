1ce8c18f992adbb2b639300ee11c8bb039e273f0
Hearen <LHearen@126.com>
2016-03-02 11:16:04 +0800

    reminder udpated with Fraction2Decimal

---
+++ LuoSonglei/week-94/ValidNumber.c
@@ -0,0 +1,20 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-02 11:12
+Description : 
+Source      : https://leetcode.com/problems/valid-number/
+*******************************************/
+/*
+ * 0xf and 0xF -> false;
+ * 07 -> true;
+ * 10e300 -> true;
+ * 10e-300 -> true;
+ * 10000000000.0000 -> true;
+ * 0000 -> true;
+ */
+#include <bool.h>
+bool isNumber(char* s)
+{
+     return true;
+}
