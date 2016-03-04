931db30e6d75cd859bd1cd4bae3b01c1c712d401
Hearen <LHearen@126.com>
2016-03-04 09:58:46 +0800

    PalindromePartitioning.c updated with comments

--- LuoSonglei/week-94/PalindromePartitioning.c
+++ LuoSonglei/week-94/PalindromePartitioning.c
@@ -8,32 +8,35 @@ Source      : https://leetcode.com/problems/palindrome-partitioning/
 #include <stdlib.h>
 void traverse(char* s, int len, int begin, char** stack, int top, char**** arrs, int** colSizes, int* returnSize)
 {
-    if(begin == len)
+    if(begin == len) //there is nothing left, collect the strings of a set;
     {
         *returnSize += 1;
         *colSizes = (int*)realloc(*colSizes, sizeof(int)*(*returnSize));
-        (*colSizes)[*returnSize-1] = top+1;
+        int size = top+1;
+        (*colSizes)[*returnSize-1] = size;
         *arrs = (char***)realloc(*arrs, sizeof(char**)*(*returnSize));
-        (*arrs)[*returnSize-1] = (char**)malloc(sizeof(char*)*(top+1));
-        for(int i = 0; i <= top; i++)
+        (*arrs)[*returnSize-1] = (char**)malloc(sizeof(char*)*size);
+        for(int i = 0; i < size; i++)
             (*arrs)[*returnSize-1][i] = stack[i];
         return ;
     }
-    for(int i = begin; i < len; i++)
+    for(int i = begin; i < len; i++) //check each string that begin with s[begin];
     {
         int l=begin, r=i;
         while(l<r && s[l]==s[r]) l++, r--;
-        if(l >= r)
+        if(l >= r) //it's a palindrome;
         {
             int size = i-begin+1;
             char *t = (char*)malloc(sizeof(char)*(size+1));
             *t = '\0';
             strncat(t, s+begin, size);
             stack[top+1] = t;
-            traverse(s, len, i+1, stack, top+1, arrs, colSizes, returnSize);
+            traverse(s, len, i+1, stack, top+1, arrs, colSizes, returnSize); //collect the left;
         }
     }
 }
+
+//AC - 8ms;
 char*** partition(char* s, int** colSizes, int* returnSize)
 {
     if(!*s) return NULL;
