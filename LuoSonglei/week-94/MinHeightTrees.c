d3892d4e5bfda6cacac8a2376c60f8b44fa5f01a
Hearen <LHearen@126.com>
2016-03-05 21:47:34 +0800

    MinHeightTrees.c set should be handled in C

---
+++ LuoSonglei/week-94/MinHeightTrees.c
@@ -0,0 +1,27 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-05 21:47
+Description : For a undirected graph with tree characteristics, we can choose any node as the root. The result graph is then a rooted tree. Among all possible rooted trees, those with minimum height are called minimum height trees (MHTs). Given such a graph, write a function to find all the MHTs and return a list of their root labels.
+
+Format
+The graph contains n nodes which are labeled from 0 to n - 1. You will be given the number n and a list of undirected edges (each edge is a pair of labels).
+
+You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+
+Example 1:
+
+Given n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+
+0
+|
+1
+/ \
+2   3
+return [1]
+Source      : https://leetcode.com/problems/minimum-height-trees/
+*******************************************/
+int* findMinHeightTrees(int n, int** edges, int rSize, int cSize, int* returnSize)
+{
+
+}
