2dd792dfb24cadc483f9480f27430e516fd87573
Hearen <LHearen@126.com>
2016-03-06 21:33:18 +0800

    138-CopyListWithRandomPointerOptimized.c updated with comments

---
+++ LuoSonglei/week-94/138-CopyListWithRandomPointerOptimized.c
@@ -0,0 +1,51 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-06 20:41
+Description : A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+Source      : https://leetcode.com/problems/copy-list-with-random-pointer/
+*******************************************/
+#include <stdlib.h>
+struct RandomListNode
+{
+    int label;
+    struct RandomListNode *next;
+    struct RandomListNode *random;
+};
+
+struct RandomListNode* copyRandomList(struct RandomListNode *head)
+{
+    if(!head) return NULL;
+    struct RandomListNode *newHead=NULL, *oldNode=head, *newNode=NULL, *preNewNode=NULL;
+    while(oldNode) //copy the node and place the new node just after the original one;
+    {
+        newNode = (struct RandomListNode*)malloc(sizeof(struct RandomListNode));
+        newNode->next = oldNode->next;
+        newNode->label = oldNode->label;
+        newNode->random = NULL;
+        oldNode->next = newNode;
+        oldNode = oldNode->next->next;
+    }
+    oldNode = head;
+    while(oldNode) //using the adjacent attribute to find the random pointer;
+    {
+        if(oldNode->random)
+            oldNode->next->random = oldNode->random->next;
+        oldNode = oldNode->next->next;
+    }
+    
+    newHead = head->next;
+    oldNode = head;
+    while(oldNode) //splitting the two merged list into two independent ones restoring the original list;
+    {
+        newNode = oldNode->next;
+        oldNode->next = newNode->next;
+        if(preNewNode)
+            preNewNode->next = newNode;
+        preNewNode = newNode;
+        oldNode = oldNode->next;
+    }
+    return newHead;
+}
