395b23b2beb4ac2cfe6c837e026b845d61be1e73
Hearen <LHearen@126.com>
2016-03-07 08:54:33 +0800

    138-CopyListWithRandomPointerOptimized.c

--- LuoSonglei/week-94/138-CopyListWithRandomPointerOptimized.c
+++ LuoSonglei/week-94/138-CopyListWithRandomPointerOptimized.c
@@ -15,7 +15,7 @@ struct RandomListNode
     struct RandomListNode *random;
 };
 
-struct RandomListNode* copyRandomList(struct RandomListNode *head)
+struct RandomListNode* copyRandomList0(struct RandomListNode *head)
 {
     if(!head) return NULL;
     struct RandomListNode *newHead=NULL, *oldNode=head, *newNode=NULL, *preNewNode=NULL;
@@ -45,6 +45,45 @@ struct RandomListNode* copyRandomList(struct RandomListNode *head)
         if(preNewNode)
             preNewNode->next = newNode;
         preNewNode = newNode;
+        oldNode = oldNode->next;
+    }
+    return newHead;
+}
+
+struct RandomListNode* copyRandomList(struct RandomListNode *head)
+{
+    if(!head) return NULL;
+    struct RandomListNode *newHead=NULL, *oldNode=head, *newNode=NULL;
+    while(oldNode) //copy the node and place the new node just after the original one;
+    {
+        newNode = (struct RandomListNode*)malloc(sizeof(struct RandomListNode));
+        newNode->next = oldNode->next;
+        newNode->label = oldNode->label;
+        newNode->random = NULL;
+        oldNode->next = newNode;
+        oldNode = oldNode->next->next;
+    }
+    oldNode = head;
+    while(oldNode) //using the adjacent attribute to find the random pointer;
+    {
+        if(oldNode->random)
+            oldNode->next->random = oldNode->random->next;
+        oldNode = oldNode->next->next;
+    }
+    
+    newHead = head->next;
+    newNode = head->next;
+    oldNode = head;
+    while(oldNode) //splitting the two merged list into two independent ones restoring the original list;
+    {
+        oldNode->next = oldNode->next->next;
+        if(oldNode->next == NULL)
+        {
+            newNode->next = NULL;
+            break;
+        }
+        newNode = newNode->next->next;
+        newNode = newNode->next;
         oldNode = oldNode->next;
     }
     return newHead;
