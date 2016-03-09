07ef1cf9eb6d650ee921f6b03f7d3c40139920f9
Hearen <LHearen@126.com>
2016-03-09 08:21:49 +0800

    147-InsertionSortList.c updated with comments

--- LuoSonglei/week-95/147-InsertionSortList.c
+++ LuoSonglei/week-95/147-InsertionSortList.c
@@ -13,21 +13,51 @@ struct ListNode
 };
 
 //AC - 68ms;
-struct ListNode* insertionSortList(struct ListNode* head)
+struct ListNode* insertionSortList0(struct ListNode* head)
 {
     struct ListNode* newHead = (struct ListNode*)malloc(sizeof(struct ListNode));
-    newHead->next = NULL;
+    newHead->next = NULL; //in case of uncertainty;
     struct ListNode *p=head, *next, *pre=newHead;
+    while(p)
+    {
+        next = p->next; //store the next node;
+        p->next = NULL;
+        while(pre->next && pre->next->val<p->val)
+            pre = pre->next;
+        p->next = pre->next; //point to the next node of the current inserting position;
+        pre->next = p; 
+        pre = newHead; //start over from the very beginning;
+        p = next; //move to the next node;
+    }
+    return newHead->next;
+}
+
+//AC - 12ms;
+struct ListNode* insertionSortList(struct ListNode* head)
+{
+    if(!head || !head->next) return head; //we have to handle the corner case now, since we adopted last and check head->val the first round;
+    struct ListNode* newHead = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newHead->next = NULL; //in case of uncertainty;
+    newHead->val = head->val-1; //to update the last for the first insertion;
+    struct ListNode *p=head, *next, *pre=newHead, *last=newHead;
     while(p)
     {
         next = p->next;
         p->next = NULL;
-        while(pre->next && pre->next->val<p->val)
-            pre = pre->next;
-        p->next = pre->next;
-        pre->next = p;
-        p = next;
-        pre = newHead;
+        if(last->val < p->val)
+        {
+            last->next = p;
+            last = p;
+        }
+        else
+        {
+            while(pre->next && pre->next->val<p->val)
+                pre = pre->next;
+            p->next = pre->next; //point to the next node of the current inserting position;
+            pre->next = p; 
+            pre = newHead; //start over from the very beginning;
+        }
+        p = next; //move to the next node;
     }
     return newHead->next;
 }
