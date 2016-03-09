9c02521c8aace5f831a4df89ed37385d216087c1
Hearen <LHearen@126.com>
2016-03-09 20:59:04 +0800

    324-WiggleSort2.c updated with comments

--- LuoSonglei/week-95/324-WiggleSort2.c
+++ LuoSonglei/week-95/324-WiggleSort2.c
@@ -41,21 +41,22 @@ void sort(int* nums, int begin, int end)
 }
 
 //AC - 40ms;
+//https://leetcode.com/discuss/76965/3-lines-python-with-explanation-proof
 void wiggleSort(int* nums, int size)
 {
-    sort(nums, 0, size-1);
+    sort(nums, 0, size-1); //using quick sort to sort the array first;
     int *arr = (int*)malloc(sizeof(int)*size);
     for(int i = 0; i < size; i++)
         arr[i] = nums[i];
-    int small=(size-1)/2;
-    int big = size-1;
-    int index = size-1;
-    if(size%2 == 0)
-        nums[index--] = arr[big--];
+    int small= 0; //the first of smallers;
+    int big = (size-1)/2+1; //the first of biggers;
+    int index = size-1; //start to fill in reverse direction: from right to left;
+    if(size%2 == 0) //if the size is even then the last should be indexed by odd size-1, so place the bigger one in odd position size-1;
+        nums[index--] = arr[big++];
     while(index > -1)
     {
-        nums[index--] = arr[small--];
-        if(index > -1)
-            nums[index--] = arr[big--];
+        nums[index--] = arr[small++];
+        if(index > -1) //in case of "underflow";
+            nums[index--] = arr[big++];
     }
 }
