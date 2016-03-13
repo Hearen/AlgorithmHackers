e0add8627369de5282fe708a73273226a6a4c3fd
Hearen <LHearen@126.com>
2016-03-13 16:17:13 +0800

    139-WordBreak.c updated with more advanced solution

--- LuoSonglei/week-95/139-WordBreak.c
+++ LuoSonglei/week-95/139-WordBreak.c
@@ -16,19 +16,10 @@ Source      : https://leetcode.com/problems/word-break/
 //TLE
 //"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
 //["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
-bool wordBreak0(char* s, char** dict, int size)
-{
-    int min = strlen(dict[0]), max = 0;
-    for(int i = 0; i < size; i++)
-    {
-        int len = strlen(dict[i]);
-        if(len < min) min = len;
-        if(len > max) max = len;
-    }
-    return traverse(s, 0, strlen(s), min, max, dict, size);
-}
+//Using naive DFS will result in this case, try to record the status information;
 
-bool wordBreak(char* s, char** dict, int size)
+//AC - BFS - 0ms
+bool wordBreak0(char* s, char** dict, int size)
 {
     int min=INT_MAX, max=0; //compare only in a limited range of length of the substring actually;
     int* lens = (int*)malloc(sizeof(int)*size); //used to compare substring with words;
@@ -62,6 +53,50 @@ bool wordBreak(char* s, char** dict, int size)
                 {
                     int next = start+j;
                     queue[++end] = next;
+                    if(next == len)
+                        return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+//AC - DFS - 0ms;
+bool wordBreak(char* s, char** dict, int size)
+{
+    int min=INT_MAX, max=0; //compare only in a limited range of length of the substring actually;
+    int* lens = (int*)malloc(sizeof(int)*size); //used to compare substring with words;
+    int lSize = 0;
+    for(int i = 0; i < size; i++)
+    {
+        int len = strlen(dict[i]);
+        lens[lSize++] = len;
+        if(len < min) min = len;
+        if(len > max) max = len;
+    }
+    int len = strlen(s);
+    bool* visited = (bool*)malloc(sizeof(bool)*len); //used to record whether the index visited or not;
+    memset(visited, 0, len*sizeof(bool));
+    int* stack = (int*)malloc(sizeof(int)*len*100); //avoid parallel stacks' interdenpendency;
+    int top = -1;
+    stack[++top] = 0;
+    while(top > -1)
+    {
+        int start = stack[top--]; 
+        if(!visited[start]) //unvisited so far;
+        {
+            visited[start] = true; //label it as visited;
+            for(int j = min; j <= MIN(max, len-start); j++) //check different length of substringfrom the start index;
+            {
+                int i = 0;
+                for(; i < size; i++)
+                    if(lens[i]==j && strncmp(dict[i], s+start, j)==0)
+                        break;
+                if(i != size)
+                {
+                    int next = start+j;
+                    stack[++top] = next;
                     if(next == len)
                         return true;
                 }
