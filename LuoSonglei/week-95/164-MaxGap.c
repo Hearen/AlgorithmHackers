841a60d25330fab25c527c8ee3d0b39c3e04f701
Hearen <LHearen@126.com>
2016-03-13 09:39:37 +0800

    164-MaxGap.c added with nothing

---
+++ LuoSonglei/week-95/164-MaxGap.c
@@ -0,0 +1,17 @@
+/*******************************************
+Author      : LHearen
+E-mail      : LHearen@126.com
+Time        : 2016-03-13 09:39
+Description : Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Try to solve it in linear time/space.
+
+Return 0 if the array contains less than 2 elements.
+
+You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+Source      : https://leetcode.com/problems/maximum-gap/
+*******************************************/
+int maximumGap(int* nums, int size)
+{
+
+}
