0f051266f5b3d0e68906001d9c681bbc31cb3fef
Hearen <LHearen@126.com>
2016-03-13 15:42:41 +0800

    139-WordBreak.c updated with comments

--- LuoSonglei/week-95/139-WordBreak.c
+++ LuoSonglei/week-95/139-WordBreak.c
@@ -13,29 +13,11 @@ Source      : https://leetcode.com/problems/word-break/
 *******************************************/
 #include <cstdbool.h>
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
-bool traverse(char* s, int start, int len, int min, int max, char** dict, int size)
-{
-    if(start == len)
-        return true;
-    int top = MIN(len-start, max);
-    for(int i = min; i < top; i++) //the length of the substring starting from index start;
-    {
-        int j = 0;
-        for(; j < size; j++) //check whether the substring is a word in the dictionary;
-            if(strlen(dict[j])==i && strncmp(s+start, dict[j], i)==0)
-                break;
-        if(j!=size && traverse(s, start+i, len, min, max, dict, size)) return true;
-    }
-    return false;
-}
-
 //TLE
 //"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
 //["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
-bool wordBreak(char* s, char** dict, int size)
+bool wordBreak0(char* s, char** dict, int size)
 {
-    if(*s=='\0') return true;
-    if(size == 0) return false;
     int min = strlen(dict[0]), max = 0;
     for(int i = 0; i < size; i++)
     {
@@ -44,4 +26,47 @@ bool wordBreak(char* s, char** dict, int size)
         if(len > max) max = len;
     }
     return traverse(s, 0, strlen(s), min, max, dict, size);
+}
+
+bool wordBreak(char* s, char** dict, int size)
+{
+    int min=INT_MAX, max=0; //compare only in a limited range of length of the substring actually;
+    int* lens = (int*)malloc(sizeof(int)*size); //used to compare substring with words;
+    int lSize = 0;
+    for(int i = 0; i < size; i++)
+    {
+        int len = strlen(dict[i]);
+        lens[lSize++] = len;
+        if(len < min) min = len;
+        if(len > max) max = len;
+    }
+    int len = strlen(s);
+    bool* visited = (bool*)malloc(sizeof(bool)*len); //used to record whether the index visited or not;
+    memset(visited, 0, len*sizeof(bool));
+    int* queue = (int*)malloc(sizeof(int)*len*100); //avoid parallel stacks' interdenpendency;
+    int begin=0, end=-1;
+    queue[++end] = 0;
+    while(end-begin > -1)
+    {
+        int start = queue[begin++]; 
+        if(!visited[start]) //unvisited so far;
+        {
+            visited[start] = true; //label it as visited;
+            for(int j = min; j <= MIN(max, len-start); j++) //check different length of substringfrom the start index;
+            {
+                int i = 0;
+                for(; i < size; i++)
+                    if(lens[i]==j && strncmp(dict[i], s+start, j)==0)
+                        break;
+                if(i != size)
+                {
+                    int next = start+j;
+                    queue[++end] = next;
+                    if(next == len)
+                        return true;
+                }
+            }
+        }
+    }
+    return false;
 }
